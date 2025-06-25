b64533535b06e51d378565396c84314bc0823e6c
Angel Daniel Morales <angelmorales@meta.com>
2025-06-25 15:09:55 -0700

    created supabase client

---
+++ front-end/next_js_app/prisma/lib/prisma.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+//THIS FILE CREATES ONE INSTANCE OF THE PRISMA CLIENT, ALL OTHER FILES USE THIS INSTANCE
---
+++ front-end/next_js_app/prisma/seed.ts
@@ -0,0 +1,22 @@
+//purpose of the seed file is to populate database w/ inital data
+import { PrismaClient } from "../src/generated/prisma";
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.upsert({
+    // upsert updates user if present, creats it if not
+    where: { email: "admorales3@wisc.edu" },
+    update: {
+      user_name: "Angel Morales",
+      password: "1234",
+    },
+    create: {
+      email: "admorales3@wisc.edu",
+      user_name: "Angel Morales",
+      password: "1234",
+    },
+  });
+  console.log(user);
+}
+
+main().then(() => prisma.$disconnect());
